49a2596fc9358d6c0ea86e57330465607301a729
ddddabi <ii887742@gmail.com>
2025-07-15 09:05:16 +0900

    feat: 학생 목록 View 추가 및 Controller, Student 수정

---
+++ src/view/StudentPrinter.java
@@ -0,0 +1,20 @@
+package view;
+
+import static view.AnsiColor.BOLD;
+import static view.AnsiColor.CYAN;
+import static view.AnsiColor.RESET;
+
+import java.util.List;
+
+import database.Student;
+
+public class StudentPrinter {
+    public void print(Student stu) {
+        System.out.println("번호: " + stu.getNo());
+        System.out.println("이름: " + stu.getName());
+        System.out.println("출생연도: " + stu.getAge());
+        System.out.println("MBTI: " + stu.getMbti());
+        System.out.println("안경 여부: " + (stu.isGlass() ? "착용" : "미착용"));
+        System.out.println("--------------------------");
+    }
+}
